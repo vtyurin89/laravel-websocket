502fe8b101295c26b695bda95c2f6d3cceedc872
Jafes <vlad89main@gmail.com>
2024-08-21 09:53:09 +0300

    ADDED first migrations

---
+++ app/Models/Like.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Like extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    public $timestamps = true;
+
+    protected $table = 'likes';
+    protected $guarded = [];
+
+}
